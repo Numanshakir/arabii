90b516c0299bb8c303ec73150b44b5d68e44cefc
Numan Shakir <numanshakir248@gmail.com>
2025-10-30 14:50:13 +0500

    Initial Commit

---
+++ arabiiTests/arabiiTests.swift
@@ -0,0 +1,16 @@
+//
+//  arabiiTests.swift
+//  arabiiTests
+//
+//  Created by Haroon Awan on 30/10/2025.
+//
+
+import Testing
+
+struct arabiiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
